17c17bbdf4448496b105c5844ee3f4ca543817b1
Tomi <67764154+tlapietra98@users.noreply.github.com>
2024-12-07 14:50:07 -0300

    clase 09
    cosas de la clase 09

--- src/app.js
+++ src/app.js
@@ -1,13 +1,59 @@
 import express from "express";
+import handlebars from "express-handlebars";
 import productsRoutes from "./router/products.routes.js";
 import cartsRoutes from "./router/carts.routes.js";
 
-const PORT = 8080
+const PORT = 8080;
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Handlebars config
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    const { name } = req.query;
+
+    res.render("home", { name, styles: "home.css" });
+});
+
+app.get("/users", (req, res) => {
+    let users = [
+        {
+            name: "Tomi",
+            lastName: "La Pietra",
+        },
+        {
+            name: "Juan",
+            lastName: "Perez",
+        },
+        {
+            name: "Pedro",
+            lastName: "Sanchez",
+        },
+        {
+            name: "Ana",
+            lastName: "Diaz",
+        },
+    ];
+
+    res.render("users", { users });
+});
+
+app.get("/admin", (req, res) => {
+    let user = {
+        name: "Tomi",
+        lastName: "La Pietra",
+        isAdmin: false,
+    }
+    res.render("admin", user);
+})
+
 
 app.use("/api/products", productsRoutes);
 app.use("/api/carts", cartsRoutes);
